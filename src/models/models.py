1e272afc6598bb2a1366c10df3d1efd48a47c2cc
alinesccp <alinesccparaujo@gmail.com>
2021-04-13 15:11:28 -0300

    modelos

---
+++ src/models/models.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel
+from typing import Optional, List
+
+
+class user (BaseModel):    
+    id: Optional[str] = None
+    nome: str
+    telefone: str
+    meus_podutos: List[Produtos]
+    minhas_vendas: List[Pedido]
+    meus_pedidos: List[Pedido]
+
+
+class Produto(BaseModel):
+    usuario: Usuario
+    id: Optional[str] = None
+    nome: str
+    detalhes: str
+    preco: float
+    disponivel: bool = False
+
+class Pedido(BaseModel):
+    id: Optional[str] = None
+    usuario = Usuario
+    produto = Produto
+    quantidade = int
+    entrega: boll = True
+    endereco: str 
+    observacoes: Optional[str] = "Sem observações"
+
+
+    
